3b4256a12794c05faa701dc21e79e6d43be62536
Nicolas <nstagno@eborne.com.uy>
2023-03-24 19:02:28 -0300

    Initial repository - project PHP Library

---
+++ src/classes/SurrealDB.php
@@ -0,0 +1,11 @@
+<?php 
+namespace SurrealDB\classes;
+use SurrealDB\interfaces\ISurrealDB;
+
+
+class  SurrealDB implements ISurrealDB
+{
+    
+}
+
+?>
---
+++ src/interfaces/ISurrealDB.php
@@ -0,0 +1,8 @@
+<?php
+namespace SurrealDB\interfaces;
+
+interface ISurrealDB{
+    
+} 
+
+?>
