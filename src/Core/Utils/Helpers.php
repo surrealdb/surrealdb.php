cecfcca022da10c16eed756ab9dd8e5801d158bb
Beau <beaudenheijer@gmail.com>
2024-05-07 23:55:44 +0200

    chore: added escape ident helper function

--- src/Core/Utils/Helpers.php
+++ src/Core/Utils/Helpers.php
@@ -17,14 +17,34 @@ final class Helpers
 
     public static function parseTarget(array $target): array
     {
-        if(!array_key_exists("namespace", $target)) {
+        if (!array_key_exists("namespace", $target)) {
             $target["namespace"] = null;
         }
 
-        if(!array_key_exists("database", $target)) {
+        if (!array_key_exists("database", $target)) {
             $target["database"] = null;
         }
 
         return [$target["namespace"], $target["database"]];
+    }
+
+    public static function escapeIdent(string $ident): string
+    {
+        $len = strlen($ident);
+
+        for ($i = 0; $i < $len; $i++) {
+            $code = ord($ident[$i]);
+            if (
+                !($code > 47 && $code < 58) && // numeric (0-9)
+                !($code > 64 && $code < 91) && // upper alpha (A-Z)
+                !($code > 96 && $code < 123) && // lower alpha (a-z)
+                !($code === 95) // underscore (_)
+            ) {
+                $str = str_replace("⟩", "\⟩", $ident[$i]);
+                return "⟨" . $str . "⟩";
+            }
+        }
+
+        return $ident;
     }
 }
