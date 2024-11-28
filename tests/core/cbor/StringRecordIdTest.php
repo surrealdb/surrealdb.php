2b5b07f2dd6751a7d61c42858963d26b30da18a4
Beau <beaudenheijer@gmail.com>
2024-11-28 21:50:15 +0100

    chore: improved StringRecordId class (#14)

--- tests/core/cbor/StringRecordIdTest.php
+++ tests/core/cbor/StringRecordIdTest.php
@@ -1,10 +1,11 @@
 <?php
 
 use Beau\CborPHP\exceptions\CborException;
+use PHPUnit\Framework\TestCase;
 use Surreal\Cbor\CBOR;
 use Surreal\Cbor\Types\Record\StringRecordId;
 
-class StringRecordIdTest extends \PHPUnit\Framework\TestCase
+class StringRecordIdTest extends TestCase
 {
     const RECORD_ID_STRING = "record:some-record";
 
@@ -26,5 +27,37 @@ class StringRecordIdTest extends \PHPUnit\Framework\TestCase
         $this->assertEquals('"' . self::RECORD_ID_STRING . '"', json_encode($stringRecordId));
 
         $this->assertEquals(self::RECORD_ID_STRING, strval($stringRecordId));
+    }
+
+    public function testInstantiateStringRecordId()
+    {
+        $stringRecordId = new StringRecordId(self::RECORD_ID_STRING);
+        $this->assertEquals(self::RECORD_ID_STRING, $stringRecordId->toString());
+    }
+
+    public function testEquals()
+    {
+        $strid1 = new StringRecordId(self::RECORD_ID_STRING);
+        $strid2 = new StringRecordId(self::RECORD_ID_STRING);
+
+        $this->assertTrue($strid1->equals($strid2));
+
+        $strid3 = new StringRecordId("record:some-other-record-1");
+        $this->assertFalse($strid1->equals($strid3));
+    }
+
+    public function testJSONSerialize()
+    {
+        $strid = new StringRecordId(self::RECORD_ID_STRING);
+        $this->assertEquals('"' . self::RECORD_ID_STRING . '"', json_encode($strid));
+
+        // reparsing the JSON should give the same string
+        $this->assertEquals(self::RECORD_ID_STRING, json_decode(json_encode($strid), true));
+    }
+
+    public function testCreate()
+    {
+        $strid = StringRecordId::create(self::RECORD_ID_STRING);
+        $this->assertEquals(self::RECORD_ID_STRING, $strid->toString());
     }
 }
